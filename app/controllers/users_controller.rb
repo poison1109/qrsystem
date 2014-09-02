9059da9545872ed5f956d5519a2d69c793f0f9d4
poison1109 <tomohisa@tokyosmith.asia>
2014-09-02 11:02:54 +0900

    細かい修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user
+
 	def index
 		@search = User.search(params[:q])
 		@users = @search.result.page(params[:page]).per(3)
-		if current_user.admin_flag != 1
-			redirect_to user_videos_path(current_user.id)
+		if @user.admin_flag != 1
+			redirect_to user_videos_path(@user.id)
 		end
 	end
+
+  private
+    def set_user
+      @user = current_user
+    end
 end
