5f85d0c9b391c260f679aecfabebc0676dda389b
poison1109 <tomohisa@tokyosmith.asia>
2014-09-04 22:18:45 +0900

    細かい修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,7 +5,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
     build_resource(sign_up_params)
-
+    resource.admin_flag = 0
     resource_saved = resource.save
     yield resource if block_given?
     if resource_saved
